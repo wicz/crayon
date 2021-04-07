65e592bded97d468f0ad438d3dcb82a01a246081
David Kariukí <davidkariuki@users.noreply.github.com>
2021-04-07 09:59:14 +0100

    Bump to 4.1.6

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
-export * from './router'
-export * from './group'
-export * from './request'
-export * from './response'
-export * from './types'
+export * from './platform/router'
+export * from './platform/mount'
+export * from './platform/history'
+export * from './platform/locator'
+export * from './platform/route-map'
+export * from './platform/shared-state'
+export * from './platform/router'
+export * from './sdk'
 
-import { create } from './router'
-export default { create }
+import { create, group } from './sdk'
+import { 
+  Router as OGRouter, 
+  Group as OGGroup,
+} from './platform/router'
+
+export const crayon = {
+  create, 
+  group
+}
+
+export declare module crayon {
+  export type Router = OGRouter
+  export type Group = OGGroup
+}
+
+export default crayon
